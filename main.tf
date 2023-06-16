4d999b20e9ecc929977339780d6dde9856c91525
Sarah Whalley <s.whalley1130@gmail.com>
2023-06-16 12:23:22 -0600

    Connected to Azure; terraform plan & apply incredibly slow, and still unable to create new resource

--- main.tf
+++ main.tf
@@ -7,28 +7,9 @@ terraform {
   }
 }
 
-variable "CLIENT_ID" {
-  type=string
-}
-
-variable "CLIENT_SECRET" {
-  type=string
-}
-
-variable "TENANT_ID" {
-  type=string
-}
-
-variable "SUBSCRIPTION_ID" {
-  type=string
-}
 
 provider "azurerm" {
-    subscription_id = var.SUBSCRIPTION_ID
-    client_id       = var.CLIENT_ID
-    client_secret   = var.CLIENT_SECRET
-    tenant_id       = var.TENANT_ID
-    features {}
+  features {}
 }
 
 resource "azurerm_resource_group" "minecraft_server" {
